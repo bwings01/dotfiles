54c386f01620c84dff47b7441fa6b10094d9de57
bwings01 <braedenmwings@gmail.com>
2025-10-15 17:53:28 -0400

    Potential fix

--- .config/.scripts/github-launch.sh
+++ .config/.scripts/github-launch.sh
@@ -5,7 +5,7 @@ hyprctl dispatch workspace 4
 if ! pgrep -f "chromium.*github" >/dev/null; then
   nohup env GDK_BACKEND=x11 chromium \
     --user-data-dir=/home/braeden/.config/chromium-endeavour \
-    --app="https//www.github.com" \
+    --app="https//github.com" \
     --class=github --name=github --new-window \
     >/dev/null 2>&1 &
 else
