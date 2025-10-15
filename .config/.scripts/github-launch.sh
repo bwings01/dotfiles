c45443b0248d2cfb726697e12a32ca152d6f5630
bwings01 <braedenmwings@gmail.com>
2025-10-15 18:56:26 -0400

    Holy shit it actually works

--- .config/.scripts/github-launch.sh
+++ .config/.scripts/github-launch.sh
@@ -1,13 +1,17 @@
-#!/usr/bin/env bash
 
+#!/usr/bin/env bash
 # --- Launch GitHub ---
 hyprctl dispatch workspace 4
-if ! pgrep -fa "chromium.*github.com" >/dev/null; then
-  nohup env GDK_BACKEND=x11 chromium \
-    --user-data-dir=/home/braeden/.config/chromium-endeavour \
-    --app="https//github.com" \
-    --class=github --name=github --new-window \
-    >/dev/null 2>&1 &
+
+# If GitHub app not open, launch it
+if ! pgrep -fa "chromium.*https://github.com" >/dev/null; then
+    nohup env GDK_BACKEND=x11 chromium \
+        --user-data-dir=/home/braeden/.config/chromium-main \
+        --app="https://github.com" \
+        --class=github --name=github --new-window \
+        >/dev/null 2>&1 &
 else
-  hyprctl dispatch focuswindow "github"
+    # Focus GitHub window if it already exists
+    hyprctl dispatch focuswindow "github"
 fi
+
