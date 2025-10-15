70cc059a9ad11bc706048e0a004642057fc2bdcf
bwings01 <braedenmwings@gmail.com>
2025-10-15 18:10:59 -0400

    Fix
    this is the last one im done after this

--- .config/.scripts/github-launch.sh
+++ .config/.scripts/github-launch.sh
@@ -2,12 +2,12 @@
 
 # --- Launch GitHub ---
 hyprctl dispatch workspace 4
-if ! pgrep -fa "chromium.*github" >/dev/null; then
+if ! pgrep -fa "chromium.*github.com" >/dev/null; then
   nohup env GDK_BACKEND=x11 chromium \
     --user-data-dir=/home/braeden/.config/chromium-endeavour \
     --app="https//github.com" \
     --class=github --name=github --new-window \
     >/dev/null 2>&1 &
 else
-  hyprctl dispatch focuswindow "chromium.*github"
+  hyprctl dispatch focuswindow "github"
 fi
