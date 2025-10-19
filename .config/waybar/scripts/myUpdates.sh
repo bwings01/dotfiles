90d2539b6cfde09d918bca0247732d18cc5986d8
bwings01 <braedenmwings@gmail.com>
2025-10-18 22:23:33 -0400

    Style changes yfm

--- .config/waybar/scripts/myUpdates.sh
+++ .config/waybar/scripts/myUpdates.sh
@@ -5,5 +5,7 @@ pac_updates=$(checkupdates 2>/dev/null | wc -l)
 aur_updates=$(yay -Qu 2>/dev/null | wc -l)
 updates=$(( pac_updates + aur_updates ))
 
-# Output JSON for Waybar
-echo "{\"text\": \"$updates\", \"tooltip\": \"$updates updates available\", \"class\": \"updates\"}"
+# Only output JSON if there are updates
+if [ "$updates" -gt 0 ]; then
+    echo "{\"text\": \"$updates\", \"tooltip\": \"$updates updates available\", \"class\": \"updates\"}"
+fi
